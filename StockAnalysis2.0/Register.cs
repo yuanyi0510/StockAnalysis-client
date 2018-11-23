5b1f6ca27327bcd9421c7590374ad991c75f9db4
yuanyi0510 <1096751192@qq.com>
2018-11-23 17:30:53 +0800

    注册界面

---
+++ StockAnalysis2.0/Register.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace StockAnalysis2._0
+{
+    public partial class Register : Form
+    {
+        public Register()
+        {
+            InitializeComponent();
+        }
+    }
+}
